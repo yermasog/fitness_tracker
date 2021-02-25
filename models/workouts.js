eadf385f2a2f8e09207df94851c7e6ab4c86300a
ymasog <yer.masog@gmail.com>
2021-02-24 18:30:03 -0600

    added seed data to database

---
+++ models/workouts.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var workoutSchema = new Schema({
+    day: { type: Date, required: true },
+    excercises: [{
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance : {
+            type: Number,
+        }
+
+    }]
+});
+
+var Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
