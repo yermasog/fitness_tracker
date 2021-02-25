c8c5ee95b492f8961afccf81056f5e4f5aa5ed96
ymasog <yer.masog@gmail.com>
2021-02-24 22:41:28 -0600

    added app.get route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,20 @@
 const db = require("../models");
 const app = require("express").Router();
 
-  app.get("/api/exercise", function(req, res) {
+  app.get("/api/workouts", function(req, res) {
     db.Workout.find({}).then(function(dbWorkouts) {
       res.json(dbWorkouts);
     });
   });
+
+  app.post("/api/workouts", function(req,res) {
+      db.Workout.create(req.body)
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  })
 
 module.exports = app
